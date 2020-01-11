a4215bb1daf3836fb5a05f2841aad8ef50922a6a
ImBadnick <fuma2@hotmail.it>
2020-01-11 03:10:53 +0100

    Added typecheck, controls and binary functions
    Added typecheck, controls and binary functions

--- ProgettoPR2OCAML.ml
+++ ProgettoPR2OCAML.ml
@@ -4,6 +4,7 @@ type exp = Eint of int | Ebool of bool | Den of ide | Prod of exp * exp | Sum of
 	Eq of exp * exp | Minus of exp | IsZero of exp | Or of exp * exp | And of exp * exp | Not of exp |
 	Ifthenelse of exp * exp * exp | Let of ide * exp * exp | Fun of ide * exp | FunCall of exp * exp |
   Letrec of ide * exp * exp
+  (*Extends a dict and his operations*)
   | Dict of dictarg
   | Insert of ide * exp * exp 
   | Delete of exp * ide 
@@ -11,8 +12,8 @@ type exp = Eint of int | Ebool of bool | Den of ide | Prod of exp * exp | Sum of
   | Iterate of exp * exp
   | Fold of exp * exp
   | Filter of (ide list) * exp
-  | FunAcc of ide * ide * exp
-  | FunCallAcc of exp * exp * exp
+  | FunAcc of ide * ide * exp (*Extends binary functions*)
+  | FunCallAcc of exp * exp * exp (*Extends binary functions*)
   and dictarg = Empty | Val of ide * exp * dictarg;;
 
 (*ambiente polimorfo*)
@@ -23,20 +24,35 @@ let bind (r : 't env) (i : ide) (v : 't) = function x -> if x = i then v else ap
 
 (*tipi esprimibili*)
 type evT = Int of int | Bool of bool | String of string | Unbound | FunVal of evFun | RecFunVal of ide * evFun | Dictvalues of (ide * evT) list
-           | FunValAcc of evFunAcc
-and evFunAcc= ide * ide * exp * evT env
+           | FunValAcc of evFunAcc (*Closure of a function that gets an accomulator*)
+and evFunAcc= ide * ide * exp * evT env (*Closure of a function that gets an accomulator*)
 and evFun = ide * exp * evT env;;
 
 (*rts*)
 (*type checking*)
-let typecheck (s : string) (v : evT) : bool = match s with
+let rec typecheck (s : string) (v : evT) : bool = match s with
 	"int" -> (match v with
             Int(_) -> true 
             | _ -> false) 
   |	"bool" -> (match v with
 		           Bool(_) -> true 
                | _ -> false) 
-  | _ -> failwith("not a valid type");;
+  
+  (*Dynamic typecheck of dict --> Controls if all the elements of the dict passed as parameter have the same type*)
+  | "Dictvalues" -> (match v with
+                     Dictvalues(l) -> match l with 
+                                      [] -> true
+                                      |(id,x)::xs -> if (typecheck "int" x) then typec "int" xs
+                                                    else typec "bool" xs
+                                      |_ -> failwith("typecheck error")
+                     |_ -> false
+                     )
+  | _ -> failwith("not a valid type")
+ 
+  and typec (tp: string) (l:(ide * evT) list) : bool = 
+      (match l with
+      [] -> true
+      | (id,x)::xs -> (typecheck tp x) && (typec tp xs));;
 
 (*funzioni primitive*)
 let prod x y = if (typecheck "int" x) && (typecheck "int" y)
@@ -134,20 +150,34 @@ let rec eval (e : exp) (r : evT env) : evT = match e with
                        Dictvalues(l1) -> haskey l1 id
                        | _ -> failwith("HasKey not used on a dict")) 
     | Iterate(funz,d) -> (let x=eval d r in 
-                         match d with
-                         Dict(l1) -> Dictvalues(iterate funz l1 r)
-                         | _ -> failwith("Iterate not used on a dict")) 
+                          match x with
+                            (*Applying the function after have controlled that all elements of the dict have the same type*)
+                            Dictvalues(l1) -> if (typecheck "Dictvalues" (Dictvalues(l1))) then Dictvalues(iterate funz l1 r)
+                                              else failwith("Dynamic typecheck error")
+                             | _ -> failwith("Iterate not used on a dict")) 
     | Fold(funz,d) -> (let x=eval d r in 
-                       match d with
-                       Dict(l1) -> fold funz l1 (Int(1)) r
-                      | _ -> failwith("Fold not used on a dict")) 
-     
+                       match x with
+                       (* Checking if all elements of the dict have the same type and assigning the right accomulator*)
+                       Dictvalues(l1) -> (match funz with
+                                         FunAcc(_,_,Sum(_)) -> if (typecheck "Dictvalues" (Dictvalues(l1))) then fold funz l1 (Int(0)) r 
+                                                               else failwith("Dynamic typecheck error")
+                                       | FunAcc(_,_,Prod(_)) -> if (typecheck "Dictvalues" (Dictvalues(l1))) then fold funz l1 (Int(1)) r 
+                                                                else failwith("Dynamic typecheck error")
+                                       | FunAcc(_,_,Diff(_)) -> if (typecheck "Dictvalues" (Dictvalues(l1))) then fold funz l1 (Int(0)) r 
+                                                                else failwith("Dynamic typecheck error")
+                                       | FunAcc(_,_,And(_)) -> if (typecheck "Dictvalues" (Dictvalues(l1))) then fold funz l1 (Bool(true)) r 
+                                                               else failwith("Dynamic typecheck error")
+                                       | FunAcc(_,_,Or(_)) -> if (typecheck "Dictvalues" (Dictvalues(l1))) then fold funz l1 (Bool(false)) r 
+                                                              else failwith("Dynamic typecheck error")
+                                       | _ -> failwith("Not an admissible function"))
+                       | _ -> failwith("Fold not used on a dict")) 
+    
     | Filter(kl,d) -> (match eval d r with
                        Dictvalues(l1) -> Dictvalues(filter kl l1)
                        | _ -> failwith("Filter not used on a dict"))
           
 
-    
+    (*Function that evals the dict*)
      and evalList (l:dictarg) (amb: evT env) (a:(ide*evT) list) : (ide*evT) list = (match l with
                           Empty -> a
                           | Val(id,e,ls) -> let evalue = eval e amb
@@ -157,60 +187,53 @@ let rec eval (e : exp) (r : evT env) : evT = match e with
                                                 ))
                           | _ -> failwith("Not a dict"))
                              
+     (*Function that inserts an element at the end of the dict!*)
      and insert (l:(ide*evT) list) (id1:string) (value:evT) : (ide*evT) list= 
                           (match haskey l id1 with 
                            Bool true -> failwith("Insert errato -> Esiste gia' la chiave nel dizionario")
                            |Bool false -> l @ [(id1,value)])
                           
-  
+     (*Function that deletes an element of the dict that has the same key of the key passed in parameters*)
      and delete (l:(ide*evT) list) (id1:string) : (ide*evT) list =
                           ( match l with
                            [] -> []
                          | ((id,x)::xs) -> if id=id1 then delete xs id1 
                                                 else (delete xs id1)@[(id,x)] )
-                 
+              
+    (*Function that controls if the dict has the key passed in parameters*)
      and haskey (l:(ide*evT) list) (id1:string) : evT  = (match l with
                          [] -> Bool false
                        | (id,x)::xs -> if id1=id then Bool true
                                        else haskey xs id1) 
      
-     and iterate (funct:exp) (l1:dictarg) (amb:evT env) : (ide*evT) list = (match l1 with
-                         Empty -> []
-                         | Val(id,e,ls) -> let value = eval (FunCall(funct,e)) amb
-                                          in (id,value) :: iterate funct ls amb 
+     (*Function that applies the function passed as parameter to all the elements of the dict*)
+     and iterate (funct:exp) (l1:(ide*evT) list) (amb:evT env) : (ide*evT) list = (match l1 with
+                          [] -> []  
+                         | (id,x)::xs -> match x with
+                                           Int(u) ->let value = eval (FunCall(funct,Eint(u))) amb 
+                                                    in (id,value) :: iterate funct xs amb 
+                                           |Bool(w) ->let value = eval (FunCall(funct,Ebool(w))) amb 
+                                                     in (id,value) :: iterate funct xs amb 
+                                           |_ -> failwith("Error in iterate operation")
                          |_ -> failwith("Not a dict"))
-                     
-     and fold (funct:exp) (l1:dictarg) (a:evT) (amb:evT env)  : evT  = (match l1 with
-                                 Empty -> a
-                                 | Val(id,e,ls) -> (match a with 
-                                                    Int(u) -> fold funct ls (eval (FunCallAcc(funct,(Eint(u)),e)) amb) amb
-                                                   | _ -> failwith("Error in fold operation"))
+             
+     (*Function that does the fold*)
+     and fold (funct:exp) (l1:(ide*evT) list) (a:evT) (amb:evT env)  : evT  = (match l1 with
+                                   [] -> a
+                                 | (id,x)::xs -> (match (a,x) with 
+                                                  ((Int(u)),(Int(w))) -> fold funct xs (eval (FunCallAcc(funct,(Eint(u)),Eint(w))) amb) amb
+                                                  |((Bool(u)),(Bool(w))) -> fold funct xs (eval (FunCallAcc(funct,(Ebool(u)),Ebool(w))) amb) amb
+                                                  |((Bool(u)),(Int(w))) -> fold funct xs (eval (FunCallAcc(funct,(Ebool(u)),Eint(w))) amb) amb
+                                                  |((Int(u)),(Bool(w))) -> fold funct xs (eval (FunCallAcc(funct,(Eint(u)),Ebool(w))) amb) amb 
+                                                  | _ -> failwith ("Error in fold operation"))
                                  | _ -> failwith("Not a dict"))
-                            
+     (*Function that filters the elements of the dict -> Eliminates the elements that are not in the list passed as parameter*)                 
     and filter (kl:string list) (l:(ide*evT) list) : (ide*evT) list = (match l with
                                   [] -> [] 
                                   |(id,x)::xs ->  if (empty kl) then []
                                                   else if (List.mem id kl) then (filter kl xs)@[(id,x)]
                                                   else filter kl xs)
+    (*Function that controls if a list is empty*)
     and empty (l:string list) : bool= (match l with
                                       [] -> true
                                      | _ -> false);;
-                                      
-
-
-
-(*)                TEST            *)
-
-eval (Dict(Val("p1",Eint(10),Val("p2",Eint(20),Empty)))) (emptyenv Unbound);;
-
-eval (Insert("p3",Eint(30),(Dict(Val("p1",Eint(10),Val("p2",Eint(20),Empty)))))) (emptyenv Unbound);;
-
-eval (Delete((Dict(Val("p1",Eint(10),Val("p2",Eint(20),Val("p3",Eint(30),Empty))))),"p2")) (emptyenv Unbound);;
-
-eval (Has_key("p1",(Dict(Val("p1",Eint(10),Val("p2",Eint(20),Empty)))))) (emptyenv Unbound);;
-
-eval (Iterate(Fun("y", Sum(Den "y", Eint 101)),(Dict(Val("p1",Eint(10),Val("p2",Eint(20),Val("p3",Eint(30),Empty))))))) (emptyenv Unbound);;
-
-eval (Fold(FunAcc("acc","y",Sum(Den "acc",Sum(Den "y", Eint 5))),(Dict(Val("p1",Eint(10),Val("p2",Eint(20),Val("p3",Eint(30),Empty))))))) (emptyenv Unbound);;
-
-eval (Filter(["p1";"p2";"p3"],(Dict(Val("p1",Eint(10),Val("p2",Eint(20),Val("p3",Eint(30),Empty))))))) (emptyenv Unbound);;
